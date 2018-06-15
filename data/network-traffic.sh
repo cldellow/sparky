0961bd461870fea1225f27b9dd971ddff4afb5e8
Colin Dellow <colin@snapsort.com>
2018-06-15 15:32:15 -0400

    network-traffic
    time, ip, internal, external_out, external_in:
    
    ```
    1529091132 172.30.5.220 38646907 0 0
    1529091134 172.30.5.220 17049504 192 378
    1529091136 172.30.5.220 38718610 0 0
    ```
    
    note 2s granularity is all we get

--- data/network-traffic.sh
+++ data/network-traffic.sh
@@ -1,4 +1,68 @@
+ip=$(ip addr show eth0 | awk -F "[ /]" '/inet / {print $6}')
 mkdir -p cldellow
 s3cmd get --force s3://sortable-assets/misc/colin/iftop-1.0pre4 ./cldellow/iftop-1.0pre4
 chmod a+x cldellow/iftop-1.0pre4
-./cldellow/iftop-1.0pre4 -t -B -n -N -L 100
+./cldellow/iftop-1.0pre4 -t -B -n -N -L 100 | awk -v ip=$ip '
+BEGIN {
+  parse_next = 0
+  internal = 0
+  external_out = 0
+  external_in = 0
+  outb = 0
+  inb = 0
+  other_ip = ""
+}
+parse_next {
+#  print $0
+  inb = $3
+  other_ip = $1
+
+  multiplier = 1
+  if (inb ~ "KB") {
+    multiplier = 1024
+  }
+  if (inb ~ "MB") {
+    multiplier = 1024 * 1024
+  }
+  sub(/KB/, "", inb)
+  sub(/MB/, "", inb)
+  sub(/B/, "", inb)
+  inb = multiplier * inb
+
+  if (other_ip ~ "^172.") {
+    internal += inb
+    internal += outb
+  } else {
+    external_out += outb
+    external_in += inb
+  }
+#  printf "%s %.0f %.0f\n", other_ip, inb, outb
+
+  parse_next = 0
+}
+$0 ~ " "ip" " {
+#  print $0
+  outb = $4
+
+  multiplier = 1
+  if (outb ~ "KB") {
+    multiplier = 1024
+  }
+  if (outb ~ "MB") {
+    multiplier = 1024 * 1024
+  }
+  sub(/KB/, "", outb)
+  sub(/MB/, "", outb)
+  sub(/B/, "", outb)
+  outb = multiplier * outb
+
+  parse_next = 1
+}
+
+/^Peak rate/ {
+  printf "%s %s %.0f %.0f %.0f\n", systime(), ip, internal, external_out, external_in
+  internal = 0
+  external_out = 0
+  external_in = 0
+}
+'

1840df4605c924b182410a2a57e1c68f9c451dbd
Colin Dellow <colin@snapsort.com>
2018-06-15 13:03:40 -0400

    progress bars for jobs

--- ui/src/Job.js
+++ ui/src/Job.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
-import { VictoryChart, VictoryAxis, VictoryBar } from 'victory';
-
 
 class Job extends Component {
   constructor(props) {
     super(props);
     
-    this.state = {id: 123, description: "Doing stuff", duration: 12, tasksDone: 12, tasksActive: 4, tasksTotal: 20};
+    this.state = {
+      id: 123,
+      description: "Doing stuff",
+      duration: 12,
+      tasksDone: 12,
+      tasksActive: 4,
+      tasksTotal: 20
+    };
   }
 
+
   render() {
+    const pct = (num, denom) => {
+      const rv = Math.floor(100.0 * num / denom);
+      return rv + "%";
+    }
+
     return (
         <tr>
           <td>{this.state.id}</td>
           <td>{this.state.description}</td>
           <td>{this.state.duration}</td>
-          <td>{this.state.tasksDone} {this.state.tasksActive} {this.state.tasksTotal}</td>
+          <td>
+            <div className="progress" style={{"margin-bottom": "0", "position": "relative"}}>
+              <div className="progress-bar bg-success" style={{"width": pct(this.state.tasksDone, this.state.tasksTotal), "backgroundColor": "#3EC0FF"}}></div>
+              <div className="progress-bar bg-info" style={{"width": pct(this.state.tasksActive, this.state.tasksTotal), "backgroundColor": "#A0DFFF"}}></div>
+              <span className="task-progress">{this.state.tasksDone} / {this.state.tasksTotal}</span>
+            </div>
+ 
+{/*
+{this.state.tasksDone} {this.state.tasksActive} {this.state.tasksTotal}
+*/}
+  </td>
         </tr>
     );
   }
